49e5b03ed73ac44c5f1a3219c5037619bbcdb41e
Daniel Paul <daniel@identitysquare.ie>
2015-02-23 17:09:50 +0000

    Added comments.

--- name.asm
+++ name.asm
@@ -18,17 +18,19 @@ msg1 	BYTE "Daniel",10,13,"$"
 	
 ; start & back are labels.
 start:	mov bx,OFFSET msg1 ; OFFSET finds the number of address locations for msg1 and moves it to bx.
+
+; Register Indirect Addressing. dx is loaded with the contents of memory locations from bx...
 back: 	mov dx,[bx] ;dl=letters
 
-		
-		cmp dl,'$'
-		jz done
+		; Compare dl with '$'.
+		cmp dl,'$' ; cmp subtracts its two operands, and sets flags accordingly.
+		jz done ; jz is "Jump if zero".
 		
 		mov ah,02h
-		int 021h
+		int 021h ; Print character in dl.
 		
-		inc bx
-		jmp back
+		inc bx ; Increment bx. bx = bx + 1
+		jmp back ; Jump to the code where the label 'back' points to.
 
 done: 		nop ; Single instruction, no operation
 
